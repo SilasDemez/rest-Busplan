fc4bdb191244413a16347916a43a113cb3494dfd
SilasDemez <silas@demezgmail.com>
2022-01-27 11:06:54 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+var bodyParser = require("body-parser");
+var express = require('express');
+const { request, response } = require('express');
+const cors = require('cors');
+
+
+var app = express();
+var port = 80;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+async function connect() {
+  let conn;
+  try {
+	return await pool.getConnection();
+  //return conn;
+
+  } catch (err) {
+	throw err;
+  } finally {
+	if (conn) return conn.end();
+  }
+}
+
+async function disconnect(conn){
+  conn.end();
+}
