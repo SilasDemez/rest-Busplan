785de97d7f615caccfc5abb8a97da8441ebf83d0
SilasDemez <silas@demezgmail.com>
2022-01-27 11:15:33 +0100

    blabla

--- app.js
+++ app.js
@@ -1,33 +1,8 @@
-require('dotenv').config();
-var bodyParser = require("body-parser");
-var express = require('express');
-const { request, response } = require('express');
-const cors = require('cors');
+const express = require('express')
+const app = express()
 
+app.get('/', function (req, res) {
+  res.send('public/index.html')
+})
 
-var app = express();
-var port = 80;
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-
-app.connect();
-
-
-async function connect() {
-  let conn;
-  try {
-	return await pool.getConnection();
-  //return conn;
-
-  } catch (err) {
-	throw err;
-  } finally {
-	if (conn) return conn.end();
-  }
-}
-
-async function disconnect(conn){
-  conn.end();
-}
+app.listen(3000);
