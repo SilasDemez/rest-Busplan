d80880f29a8cef37ca97f42c7fc0a50704c11507
SilasDemez <silasdemez@gmail.com>
2022-01-31 09:30:08 +0100

    simplle write to Document

--- public/index.js
+++ public/index.js
@@ -1,8 +1,6 @@
 
-async function fetchData(){
-    let haltestelle = 66000998
-    let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${haltestelle}&mode=direct&outputFormat=JSON`
-    //'https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=Brixen%20Brixen%20Bahnhof&mode=direct&outputFormat=JSON'
+async function fetchBusescomingby(haltestellenID){
+    let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${haltestellenID}&mode=direct&outputFormat=JSON`
 
     const response = await fetch(url, {
         method: 'get',
@@ -12,7 +10,57 @@ async function fetchData(){
 
 }
 
-fetchData().then((res) => {
+async function fetchStation(ort){
+    let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${ort}&mode=direct&outputFormat=JSON`
+
+    const response = await fetch(url, {
+        method: 'get',
+    })
+    
+    return response.json();
+
+}
+
+function writeToDoc(departureList){
+    departureList.forEach(bus => {
+        // create parent div where bus info goes in
+        let div = document.createElement('div');
+        div.setAttribute('id', 'bus');
+        document.getElementById("busse").appendChild(div);
+
+        // crete a children div for every single info
+        let lineID_div = document.createElement('div');
+        let direction_div = document.createElement('div');
+        let time_div = document.createElement('div');
+
+        lineID_div.innerHTML = `${bus.servingLine.number}`;
+        direction_div.innerHTML = `${bus.servingLine.direction}`;
+        time_div.innerHTML = `${bus.dateTime.day}.${bus.dateTime.month}.${bus.dateTime.year} ${bus.dateTime.hour}:${bus.dateTime.minute}`;
+
+        lineID_div.setAttribute('id', 'lineID');
+        direction_div.setAttribute('id', 'direction');
+        time_div.setAttribute('id', 'time');
+
+        document.getElementById("bus").appendChild(lineID_div)
+        document.getElementById("bus").appendChild(direction_div)
+        document.getElementById("bus").appendChild(time_div)
+
+    });
+
+}
+
+/*
+fetchStation("Brixen").then((res) => {
     console.log(res);
-    console.log(res.servingLines.lines)
+    console.log(res.dm.points)
+    //document.getElementById("busse").innerHTML = res
+
 });
+*/
+
+fetchBusescomingby(66000998).then((res) => {
+    console.log(res);
+    console.log(res.departureList)
+    writeToDoc(res.departureList);
+});
+
