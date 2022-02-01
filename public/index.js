b501dc397038d6a286a2e69b7648d139fa748416
SilasDemez <silasdemez@gmail.com>
2022-02-01 11:15:55 +0100

    fast commit

--- public/index.js
+++ public/index.js
@@ -2,11 +2,16 @@
 async function fetchBusescomingby(haltestellenID){
     let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${haltestellenID}&mode=direct&outputFormat=JSON`
 
-    const response = await fetch(url, {
-        method: 'get',
-    })
-    
-    return response.json();
+    try{
+        const response = await fetch(url, {
+            method: 'get',
+        })
+        
+        return response.json();
+    }catch(error){
+        alert('Fehler bei der Kommunikation mitr der API. Bitte noch einmal probieren');
+    }
+
 
 }
 
@@ -21,29 +26,40 @@ async function fetchStation(ort){
 
 }
 
+
+function checkifTrain(lineID){
+    let regex = '/^[A-z]/gm'
+}
+
 function writeToDoc(departureList){
-    departureList.forEach(bus => {
+    departureList.forEach((bus, i) => {
         // create parent div where bus info goes in
         let div = document.createElement('div');
-        div.setAttribute('id', 'bus');
+        console.log(i)
+        div.setAttribute('id', `bus${i}`);
+        div.setAttribute('class', 'bus')
         document.getElementById("busse").appendChild(div);
 
         // crete a children div for every single info
-        let lineID_div = document.createElement('div');
-        let direction_div = document.createElement('div');
-        let time_div = document.createElement('div');
+        let lineID_p = document.createElement('p');
+        let direction_p = document.createElement('p');
+        let time_p = document.createElement('p');
 
-        lineID_div.innerHTML = `${bus.servingLine.number}`;
-        direction_div.innerHTML = `${bus.servingLine.direction}`;
-        time_div.innerHTML = `${bus.dateTime.day}.${bus.dateTime.month}.${bus.dateTime.year} ${bus.dateTime.hour}:${bus.dateTime.minute}`;
 
-        lineID_div.setAttribute('id', 'lineID');
-        direction_div.setAttribute('id', 'direction');
-        time_div.setAttribute('id', 'time');
+        if(checkifTrain(bus.servingLine.number)){
 
-        document.getElementById("bus").appendChild(lineID_div)
-        document.getElementById("bus").appendChild(direction_div)
-        document.getElementById("bus").appendChild(time_div)
+        }
+        lineID_p.innerHTML = `${bus.servingLine.number}`;
+        direction_p.innerHTML = `${bus.servingLine.direction}`;
+        time_p.innerHTML = `${bus.dateTime.day}.${bus.dateTime.month}.${bus.dateTime.year} ${bus.dateTime.hour}:${bus.dateTime.minute}`;
+
+        lineID_p.setAttribute('id', 'lineID');
+        direction_p.setAttribute('id', 'direction');
+        time_p.setAttribute('id', 'time');
+
+        document.getElementById(`bus${i}`).appendChild(lineID_p)
+        document.getElementById(`bus${i}`).appendChild(direction_p)
+        document.getElementById(`bus${i}`).appendChild(time_p)
 
     });
 
