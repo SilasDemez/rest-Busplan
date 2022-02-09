52f51e3f0f1d0aaf575124f2b2163bce9b9031af
Silas Demez <silasdemez@gmail.com>
2022-02-09 08:18:17 +0100

    troubleshooting

--- public/index.js
+++ public/index.js
@@ -1,220 +1,207 @@
-const teacher_url = 'https://teacher-of-the-week.herokuapp.com'
+const teacher_url = "https://teacher-of-the-week.herokuapp.com";
 
+async function fetchBusescomingby(haltestellenID) {
+  let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${haltestellenID}&mode=direct&outputFormat=JSON`;
 
-async function fetchBusescomingby(haltestellenID){
-    let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${haltestellenID}&mode=direct&outputFormat=JSON`
+  //try{
+  //console.log("Hallo")
+  const response = await fetch(url, {
+    method: "get",
+  });
 
-    //try{
-        //console.log("Hallo")
-        const response = await fetch(url, {
-            method: 'get',
-        })
-        
-        response.json().then((res) => {
-            console.log("Test")
-            console.log(res)
-            writeToDoc(res.departureList);
-        })
-        
-        // return response.json();
-    /*}catch(error){
+  response.json().then((res) => {
+    console.log("Test");
+    console.log(res);
+    writeToDoc(res.departureList);
+  });
+
+  // return response.json();
+  /*}catch(error){
         alert('Fehler bei der Kommunikation mitr der API. Bitte noch einmal probieren');
     }*/
-
-
 }
 
-async function fetchStation(ort){
-    let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${ort}&mode=direct&outputFormat=JSON`
+async function fetchStation(ort) {
+  let url = `https://efa.sta.bz.it/apb/XML_DM_REQUEST?&locationServerActive=1&stateless=1&type_dm=any&name_dm=${ort}&mode=direct&outputFormat=JSON`;
 
+  const response = await fetch(url, {
+    method: "get",
+  });
+
+  return response.json();
+}
+
+async function fetchWeatherRadar() {
+  let url = `http://daten.buergernetz.bz.it/services/weather/radarhd?lang=de&format=json`;
+
+  try {
     const response = await fetch(url, {
-        method: 'get',
-    })
-    
+      method: "get",
+    });
+
     return response.json();
-
+  } catch (error) {
+    alert(
+      "Fehler bei der Kommunikation mitr der API. Bitte noch einmal probieren"
+    );
+  }
 }
 
-async function fetchWeatherRadar(){
-    let url = `	http://daten.buergernetz.bz.it/services/weather/radarhd?lang=de&format=json`
-
-    try{
-        const response = await fetch(url, {
-            method: 'get',
-        })
-        
-        return response.json();
-    }catch(error){
-        alert('Fehler bei der Kommunikation mitr der API. Bitte noch einmal probieren');
+async function setImgofRadar(res) {
+  let durchlaufe = 0;
+  for (i = 0; ; i++) {
+    durchlaufe++;
+    await new Promise((r) => setTimeout(r, 1000));
+    document.getElementById("animation").src = res.rows[i].foregroundUrl;
+    console.log(durchlaufe);
+    if (durchlaufe == 169) {
+      console.log("Beende");
+      return 0;
+      break;
     }
-
-
-}
-
-
-async function setImgofRadar(res){
-    let durchlaufe=0
-    for(i=0;;i++){
-        durchlaufe++
-        await new Promise(r => setTimeout(r, 1000));
-        document.getElementById("animation").src = res.rows[i].foregroundUrl;
-        console.log(durchlaufe)
-        if (durchlaufe == 169) {
-            console.log("Beende")
-            return 0
-            break
-
-        }
-        if(i == res.rows.length-1){
-            await new Promise(r => setTimeout(r, 1000));
-            i = 0
-        }
+    if (i == res.rows.length - 1) {
+      await new Promise((r) => setTimeout(r, 1000));
+      i = 0;
     }
+  }
 }
 
-async function endlessRadar(){
-    while (true){
-        console.log("Loading radar");
-        await fetchWeatherRadar().then((res) => {
-            console.log(res);
-            console.log(res.rows[0].foregroundUrl);
-            setImgofRadar(res);
-        });
-
-        await new Promise(r => setTimeout(r, 183000));
-    }
-}
-
-async function setImgofPrediction(){
-    let i=0,url
-    while(true){
-        await new Promise(r => setTimeout(r, 1000));
-        if(i==33) i=0
-        url = `https://wetter.provinz.bz.it/images/wforecast${i}.jpg`
-        //console.log(url)
-        document.getElementById("forecast").src = url;
-        i++
-    }
-
-}
-
-function parseminute(minute){
-    if(minute.length == 1){
-        return "0" + minute
-    }else return minute
-}
-
-function checkifTrain(lineID){
-    let regex = '[A-z]{1,} [0-9]{1,}';
-    return lineID.match(regex);
-}
-
-function writeToDoc(departureList){
-    console.log(departureList)
-    document.getElementById("busse").innerHTML = ''
-    departureList.forEach((bus, i) => {
-        // create parent div where bus info goes in
-        let div = document.createElement('div');
-        div.setAttribute('id', `bus${i}`);
-        div.setAttribute('class', 'bus')
-        document.getElementById("busse").appendChild(div);
-
-        // crete a children div for every single info
-        let lineID_p = document.createElement('p');
-        let direction_p = document.createElement('p');
-        let time_p = document.createElement('p');
-
-        let line_ID = checkifTrain(bus.servingLine.number)
-
-
-        if(line_ID){
-            console.log("Matched!");
-            console.log(line_ID)
-            lineID_p.innerHTML = `${line_ID}`;
-        }else{
-            lineID_p.innerHTML = `${bus.servingLine.number}`;
-        }
-        direction_p.innerHTML = `${bus.servingLine.direction}`;
-        //time_p.innerHTML = `${bus.dateTime.day}.${bus.dateTime.month}.${bus.dateTime.year} ${bus.dateTime.hour}:${bus.dateTime.minute}`;
-
-        let minute = parseminute(bus.dateTime.minute)
-
-        time_p.innerHTML = `${bus.dateTime.hour}:${minute}`;
-
-        lineID_p.setAttribute('id', 'lineID');
-        direction_p.setAttribute('id', 'direction');
-        time_p.setAttribute('id', 'time');
-
-        document.getElementById(`bus${i}`).appendChild(lineID_p)
-        document.getElementById(`bus${i}`).appendChild(direction_p)
-        document.getElementById(`bus${i}`).appendChild(time_p)
-
+async function endlessRadar() {
+  while (true) {
+    console.log("Loading radar");
+    await fetchWeatherRadar().then((res) => {
+      console.log(res);
+      console.log(res.rows[0].foregroundUrl);
+      setImgofRadar(res);
     });
 
+    await new Promise((r) => setTimeout(r, 183000));
+  }
 }
 
+async function setImgofPrediction() {
+  let i = 0,
+    url;
+  while (true) {
+    await new Promise((r) => setTimeout(r, 1000));
+    if (i == 33) i = 0;
+    url = `https://wetter.provinz.bz.it/images/wforecast${i}.jpg`;
+    //console.log(url)
+    document.getElementById("forecast").src = url;
+    i++;
+  }
+}
 
+function parseminute(minute) {
+  if (minute.length == 1) {
+    return "0" + minute;
+  } else return minute;
+}
 
-/*
-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-*/
+function checkifTrain(lineID) {
+  let regex = "[A-z]{1,} [0-9]{1,}";
+  return lineID.match(regex);
+}
 
+function writeToDoc(departureList) {
+  console.log(departureList);
+  document.getElementById("busse").innerHTML = "";
+  departureList.forEach((bus, i) => {
+    // create parent div where bus info goes in
+    let div = document.createElement("div");
+    div.setAttribute("id", `bus${i}`);
+    div.setAttribute("class", "bus");
+    document.getElementById("busse").appendChild(div);
 
-function writeTeachersToDoc(teachers){
-    let arr = teachers.count;
-    arr.sort(function (a, b) {
-      return b.count - a.count;
-    });
-    console.log(arr)
-    document.getElementById("teacher_of_the_week").innerHTML = ''
-    for(i=0; i<3; i++){
-        let teacher = arr[i]
-        console.log(teacher)
+    // crete a children div for every single info
+    let lineID_p = document.createElement("p");
+    let direction_p = document.createElement("p");
+    let time_p = document.createElement("p");
 
-        let div = document.createElement('div');
+    let line_ID = checkifTrain(bus.servingLine.number);
 
-        div.setAttribute('id', `${teacher.name}`);
-        div.setAttribute('class', 'teacher')
-
-        document.getElementById("teacher_of_the_week").appendChild(div);
-
-        let name = document.createElement('p');
-        let count = document.createElement('p');
-
-        name.innerHTML = teacher.name
-        name.setAttribute('class', 'names')
-
-        count.innerHTML = teacher.count
-        count.setAttribute('class', 'counts')
-
-
-        document.getElementById(`${teacher.name}`).appendChild(name)
-        document.getElementById(`${teacher.name}`).appendChild(count)
+    if (line_ID) {
+      console.log("Matched!");
+      console.log(line_ID);
+      lineID_p.innerHTML = `${line_ID}`;
+    } else {
+      lineID_p.innerHTML = `${bus.servingLine.number}`;
     }
-}
+    direction_p.innerHTML = `${bus.servingLine.direction}`;
+    //time_p.innerHTML = `${bus.dateTime.day}.${bus.dateTime.month}.${bus.dateTime.year} ${bus.dateTime.hour}:${bus.dateTime.minute}`;
 
-async function fetchLeaderboard(){
-    console.log(`${teacher_url}/leaderboard`)
-    const response = await fetch(`${teacher_url}/leaderboard`, {
-        method: 'get'
-    });
-    
-    response.json().then((res) => {
-        console.log(res)
-        writeTeachersToDoc(res)
-    })
+    let minute = parseminute(bus.dateTime.minute);
+
+    time_p.innerHTML = `${bus.dateTime.hour}:${minute}`;
+
+    lineID_p.setAttribute("id", "lineID");
+    direction_p.setAttribute("id", "direction");
+    time_p.setAttribute("id", "time");
+
+    document.getElementById(`bus${i}`).appendChild(lineID_p);
+    document.getElementById(`bus${i}`).appendChild(direction_p);
+    document.getElementById(`bus${i}`).appendChild(time_p);
+  });
 }
 
 /*
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 */
 
-fetchBusescomingby(66001143)
-setInterval(fetchBusescomingby, 60000, 66001143)
+function writeTeachersToDoc(teachers) {
+  let arr = teachers.count;
+  arr.sort(function (a, b) {
+    return b.count - a.count;
+  });
+  console.log(arr);
+  document.getElementById("teacher_of_the_week").innerHTML = "";
+  for (i = 0; i < 3; i++) {
+    let teacher = arr[i];
+    console.log(teacher);
 
-endlessRadar()
-setImgofPrediction()
-fetchLeaderboard()
+    let div = document.createElement("div");
+
+    div.setAttribute("id", `${teacher.name}`);
+    div.setAttribute("class", "teacher");
+
+    document.getElementById("teacher_of_the_week").appendChild(div);
+
+    let name = document.createElement("p");
+    let count = document.createElement("p");
+
+    name.innerHTML = teacher.name;
+    name.setAttribute("class", "names");
+
+    count.innerHTML = teacher.count;
+    count.setAttribute("class", "counts");
+
+    document.getElementById(`${teacher.name}`).appendChild(name);
+    document.getElementById(`${teacher.name}`).appendChild(count);
+  }
+}
+
+async function fetchLeaderboard() {
+  console.log(`${teacher_url}/leaderboard`);
+  const response = await fetch(`${teacher_url}/leaderboard`, {
+    method: "get",
+  });
+
+  response.json().then((res) => {
+    console.log(res);
+    writeTeachersToDoc(res);
+  });
+}
+
+/*
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+fetchBusescomingby(66001143);
+setInterval(fetchBusescomingby, 60000, 66001143);
+
+endlessRadar();
+setImgofPrediction();
+fetchLeaderboard();
 
 /*
 fetchStation("Brixen").then((res) => {
@@ -240,3 +227,12 @@ fetchWeatherRadar().then((res) => {
 });
 
 */
+
+
+let xhr = new XMLHttpRequest();
+xhr.open('get', 'http://daten.buergernetz.bz.it/services/weather/radarhd?lang=de&format=json.com/posts/1');
+xhr.send();
+
+xhr.onload = function() {
+    console.log(xhr.response);
+};
