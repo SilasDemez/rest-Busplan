6de41f2de750ded3ce181ab16ff77e4b8dcd1262
Gavaii <61867970+Gavaii@users.noreply.github.com>
2022-02-11 17:55:51 +0100

    ✨  add preloading screen

--- public/index.js
+++ public/index.js
@@ -218,6 +218,46 @@ async function fetchLeaderboard() {
   });
 }
 
+//fetch api from https://www.fallmerayer.it/wp-json/wp/v2/posts
+async function fetchPosts() {
+  await fetch("https://www.fallmerayer.it/wp-json/wp/v2/posts")
+    .then((response) => response.json())
+    .then((data) => {
+      //get the description of each post
+      let posts = data;
+      for (let i = 0; i < posts.length; i++) {
+        let post = document.createElement("div");
+        post.setAttribute("class", "post");
+        post.setAttribute("id", `post${i}`);
+        document.getElementById("news").appendChild(post);
+        post.innerHTML = posts[i].title.rendered;
+
+        //get the id of each post
+        let id = posts[i].id;
+
+        let img_url =
+          "https://www.fallmerayer.it/wp-json/wp/v2/media?parent=" + id;
+
+        //take the img_url and post the image to the page
+        fetch(img_url)
+          .then((response) => response.json())
+          .then((data) => {
+            try {
+              let img = data[0].guid.rendered;
+              let img_tag = document.createElement("img");
+              img_tag.setAttribute("src", img);
+              img_tag.setAttribute("class", "post_image");
+              document.getElementById(`post${i}`).appendChild(img_tag);
+            } catch (error) {
+              console.log("no image");
+            }
+          });
+
+        //https://www.fallmerayer.it/wp-json/wp/v2/media?parent=7548
+      }
+    });
+}
+
 /*
 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 */
@@ -227,3 +267,4 @@ setInterval(fetchBusescomingby, 60000, 66001143);
 
 fetchLeaderboard();
 fetchWeather();
+fetchPosts();
